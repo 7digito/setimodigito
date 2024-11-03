3a50d1de8cef681d96b4a72f4619a4c52a0ea9c2
7digito <cdpereira@outlook.com>
2024-11-03 14:27:39 +0000

    voltei para dblite3

--- setimo_digito/settings.py
+++ setimo_digito/settings.py
@@ -80,14 +80,15 @@ WSGI_APPLICATION = 'setimo_digito.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
